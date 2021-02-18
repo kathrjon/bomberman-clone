be609d42783f12f9979bdc3158964ed9d64ff0e2
kathrjon <kathrjon@gmail.com>
2021-02-17 20:44:14 -0700

    Bomb animation and bomb gameobject destroys on animation completion

---
+++ Bomberman Clones/Assets/Scripts/bombbehaviour.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bombbehaviour : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void FixedUpdate(){
+        
+    }
+
+
+    void destroyBomb(){
+        Destroy(gameObject);
+    }
+}
