3aca245ff379537f07338aa52eff4bca6bac1053
kathrjon <kathrjon@gmail.com>
2021-02-20 17:39:32 -0700

    Player moves on grid, new input is only accepted when player is centered in cell, center player in current cell if no valid input is detected

--- Bomberman Clones/Assets/Scripts/playercontroller.cs
+++ Bomberman Clones/Assets/Scripts/playercontroller.cs
@@ -8,7 +8,7 @@ public class playercontroller : MonoBehaviour
     Rigidbody2D rb;
     Vector2 movement;
     Vector3 startPosition;
-    float walkSpeed = 2.0f;
+    float walkSpeed = 3.0f;
     [SerializeField] public Rigidbody2D bomb;
     [SerializeField] public Tilemap bg;
 
@@ -26,6 +26,7 @@ public class playercontroller : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
         movePlayer(horizontalInput, verticalInput, cellCenter);
 
+
         if (Input.GetKeyDown(KeyCode.E)){
             placeBomb(cellCenter);
         }
@@ -36,20 +37,41 @@ public class playercontroller : MonoBehaviour
     }
 
     void movePlayer(float horizontalInput, float verticalInput, Vector3 cellCenter){
-        if (horizontalInput > 0){
+        if (horizontalInput != 0 && rb.position.y == cellCenter.y) {
+            movePlayerHorizontal(horizontalInput, cellCenter);
+        } else if (verticalInput != 0 && rb.position.x == cellCenter.x) {
+            movePlayerVertical(verticalInput, cellCenter);
+        } else{
+            movePlayerTowardsCellCenter(cellCenter);
+        }
+    }
+
+    void movePlayerHorizontal(float horizontalInput, Vector3 cellCenter){
+        if (horizontalInput > 0)
+        {
             moveTowardsNextCell(cellCenter.x + 1, cellCenter.y);
         }
-        if (horizontalInput < 0){
+        if (horizontalInput < 0)
+        {
             moveTowardsNextCell(cellCenter.x - 1, cellCenter.y);
         }
+    }
 
-        if (verticalInput > 0){
+    void movePlayerVertical(float verticalInput, Vector3 cellCenter){
+        if (verticalInput > 0)
+        {
             moveTowardsNextCell(cellCenter.x, cellCenter.y + 1);
         }
 
-        if (verticalInput < 0){
+        if (verticalInput < 0)
+        {
             moveTowardsNextCell(cellCenter.x, cellCenter.y - 1);
         }
+    }
+
+    void movePlayerTowardsCellCenter(Vector3 cellCenter){
+        float distance = walkSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, cellCenter, distance);
     }
 
     void moveTowardsNextCell(float adjacentCellX, float adjecentCellY){
@@ -64,9 +86,10 @@ public class playercontroller : MonoBehaviour
         //       Vector3Int tilePosition = new Vector3Int((int)rb.position.x, (int)rb.position.y, 0);
         //       Tile currentTile = (Tile)bg.GetTile(tilePosition);
         //       Vector2 tileCenter = bg.cellBounds.center;
-        //       transform.position = bg.GetCellCenterWorld(cellPosition);+
-        Debug.Log("-placeBomb- transform.rotation" + transform.rotation);
+        //       transform.position = bg.GetCellCenterWorld(cellPosition);
         Debug.Log("-placeBomb- cellCenter" + cellCenter);
+        Debug.Log("-placeBomb- transform.rotation" + transform.rotation);
+        cellCenter.z = -1;
         clone = Instantiate(bomb, cellCenter, transform.rotation);
     }
 
