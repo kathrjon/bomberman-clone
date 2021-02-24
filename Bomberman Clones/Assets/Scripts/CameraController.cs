e632860aaf08d2c82383e21d567cf18121897e29
Jacob Jordan <jacobjordan94@live.com>
2021-02-23 22:33:09 -0600

    add very basic camera control

---
+++ Bomberman Clones/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField] GameObject player;
+    public Vector3Int offset = new Vector3Int(0, 0, -11);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3Int((int)player.transform.position.x + offset.x, (int)player.transform.position.y + offset.y, -11);
+        Debug.Log(offset.z);
+    }
+}
