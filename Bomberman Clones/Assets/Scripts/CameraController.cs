66791905e9f6681721ff41fb096200852badcd26
kathrjon <kathrjon@gmail.com>
2021-02-24 23:37:17 -0700

    Added camera bounds and smoothing, moved camera transform.position from update to fixed update

--- Bomberman Clones/Assets/Scripts/CameraController.cs
+++ Bomberman Clones/Assets/Scripts/CameraController.cs
@@ -8,6 +8,12 @@ public class CameraController : MonoBehaviour
 
     [SerializeField] GameObject player;
     public Vector3Int offset = new Vector3Int(0, 0, -11);
+    public float smoothSpeed = 0.005f;
+
+    [SerializeField] float leftLimit;
+    [SerializeField] float rightLimit;
+    [SerializeField] float bottomLimit;
+    [SerializeField] float topLimit;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +24,20 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = new Vector3Int((int)player.transform.position.x + offset.x, (int)player.transform.position.y + offset.y, -11);
+
         // Debug.Log(offset.z);
+    }
+
+    void FixedUpdate()
+    {
+        Vector3Int desiredPosition = new Vector3Int((int)player.transform.position.x + offset.x, (int)player.transform.position.y + offset.y, -11);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        this.transform.position = smoothedPosition;
+
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+            Mathf.Clamp(transform.position.y, bottomLimit, topLimit),
+            transform.position.z
+        );
     }
 }
