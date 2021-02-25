c4c673ab33bdfd41685c50f9c3c58736a4f4e61f
kathrjon <kathrjon@gmail.com>
2021-02-25 00:22:43 -0700

    Move player movement to fixed update, expand movement threshold to .1f along x and y axis from cell's center

--- Bomberman Clones/Assets/Scripts/playercontroller.cs
+++ Bomberman Clones/Assets/Scripts/playercontroller.cs
@@ -8,57 +8,66 @@ using BombermanTools;
 public class playercontroller : MonoBehaviour
 {
     Rigidbody2D rb;
-    Vector3 startPosition;
-    Vector3 destinationCellCenter;
+    public Vector3 startPosition;
+    public Vector3 destinationCellCenter;
+    public Vector3 cellCenter;
     float walkSpeed = 3.0f;
     [SerializeField] public Rigidbody2D bomb;
     [SerializeField] public Tilemap bg;
     [SerializeField] public LayerMask barrierLayer;
+    public float horizontalInput = 0;
+    public float verticalInput = 0;
 
 
     void Awake()
     {
         startPosition = BMTiles.GetCellCenter(transform.position, this.bg);
+        destinationCellCenter = startPosition;
         
         this.transform.position = startPosition;
         rb = GetComponent<Rigidbody2D>();
     }
 
     void Update(){
-        Vector3 cellCenter = BMTiles.GetCellCenter(transform.position, bg);
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        movePlayer(horizontalInput, verticalInput, cellCenter);
+        cellCenter = BMTiles.GetCellCenter(transform.position, bg);
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
 
         if (Input.GetKeyDown(KeyCode.E)){
             placeBomb(cellCenter);
         }
     }
 
+    private void FixedUpdate()
+    {
+        if (horizontalInput != 0 || verticalInput != 0)
+        {
+            movePlayer(horizontalInput, verticalInput, cellCenter);
+        } else if(destinationCellCenter.x != rb.position.x || destinationCellCenter.y != rb.position.y)
+        {
+            movePlayerTowardsCellCenter(destinationCellCenter);
+        }
+        
+    }
+
     void movePlayer(float horizontalInput, float verticalInput, Vector3 cellCenter){
-        if(cellCenter.x == rb.position.x && cellCenter.y == rb.position.y){
-            if (horizontalInput != 0 && rb.position.y == cellCenter.y)
-            {
+            float distanceFromCenterY = rb.position.y - cellCenter.y;
+            if (horizontalInput != 0 && Mathf.Abs(distanceFromCenterY) < .1f){
                 movePlayerHorizontal(horizontalInput, cellCenter);
             }
-            else if (verticalInput != 0 && rb.position.x == cellCenter.x)
-            {
+            float distanceFromCenterX = rb.position.x - cellCenter.x;
+            if (verticalInput != 0 && Mathf.Abs(distanceFromCenterX) < .1f){
                 movePlayerVertical(verticalInput, cellCenter);
             }
-        } else{
-            movePlayerTowardsCellCenter(destinationCellCenter);
-         }
     }
 
     void movePlayerHorizontal(float horizontalInput, Vector3 cellCenter){
-        if (horizontalInput > 0)
-        {
+        if (horizontalInput > 0){
             Vector2 destinationCellCoordinates = new Vector2(cellCenter.x + 1, cellCenter.y);
             RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, .5f, barrierLayer);
             checkCollidersBeforeMoving(destinationCellCoordinates, hit);
         }
-        if (horizontalInput < 0)
-        {
+        if (horizontalInput < 0){
             Vector2 destinationCellCoordinates = new Vector2(cellCenter.x - 1, cellCenter.y);
             RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.left, .5f, barrierLayer);
             checkCollidersBeforeMoving(destinationCellCoordinates, hit);
@@ -66,15 +75,13 @@ public class playercontroller : MonoBehaviour
     }
 
     void movePlayerVertical(float verticalInput, Vector3 cellCenter){
-        if (verticalInput > 0)
-        {
+        if (verticalInput > 0){
             Vector2 destinationCellCoordinates = new Vector2(cellCenter.x, cellCenter.y + 1);
             RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up, .5f, barrierLayer);
             checkCollidersBeforeMoving(destinationCellCoordinates, hit);
         }
 
-        if (verticalInput < 0)
-        {
+        if (verticalInput < 0){
             Vector2 destinationCellCoordinates = new Vector2(cellCenter.x, cellCenter.y - 1);
             RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, .5f, barrierLayer);
             checkCollidersBeforeMoving(destinationCellCoordinates, hit);
@@ -82,8 +89,7 @@ public class playercontroller : MonoBehaviour
     }
 
     void checkCollidersBeforeMoving(Vector2 destinationCellCoordinates, RaycastHit2D hit){
-        if (hit.collider == null)
-        {
+        if (hit.collider == null){
             moveTowardsNextCell(destinationCellCoordinates);
         }
     }
