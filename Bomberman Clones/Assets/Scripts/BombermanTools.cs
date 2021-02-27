bfd9cf05b0efd3760a5b300eee6635600173b348
kathrjon <kathrjon@gmail.com>
2021-02-26 21:58:19 -0700

    Gave bomberman circle colliders, made movement more responsive, gave bomb circle collider, moved movement functions to BMMovement class

--- Bomberman Clones/Assets/Scripts/BombermanTools.cs
+++ Bomberman Clones/Assets/Scripts/BombermanTools.cs
@@ -18,5 +18,69 @@ namespace BombermanTools
             tm.SetTile(position, tile);
         }
     }
+
+    class BMMovement
+    {
+        float walkSpeed;
+        Transform entity;
+        LayerMask barrierLayer;
+        Tilemap bg;
+
+        public void moveHorizontal(float horizontalInput, Vector3 cellCenter, float speed, Transform transform, LayerMask layer, Tilemap tilemap)
+        {
+            walkSpeed = speed;
+            barrierLayer = layer;
+            entity = transform;
+            bg = tilemap;
+            if (horizontalInput > 0)
+            {
+                Vector2 destinationCellCoordinates = new Vector2(cellCenter.x + 1, cellCenter.y);
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, .5f, barrierLayer);
+                checkCollidersBeforeMoving(destinationCellCoordinates, hit);
+            }
+            if (horizontalInput < 0)
+            {
+                Vector2 destinationCellCoordinates = new Vector2(cellCenter.x - 1, cellCenter.y);
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.left, .5f, barrierLayer);
+                checkCollidersBeforeMoving(destinationCellCoordinates, hit);
+            }
+        }
+
+        public void moveVertical(float verticalInput, Vector3 cellCenter, float speed, Transform transform, LayerMask layer, Tilemap tilemap)
+        {
+            walkSpeed = speed;
+            barrierLayer = layer;
+            entity = transform;
+            bg = tilemap;
+            if (verticalInput > 0)
+            {
+                Vector2 destinationCellCoordinates = new Vector2(cellCenter.x, cellCenter.y + 1);
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up, .5f, barrierLayer);
+                checkCollidersBeforeMoving(destinationCellCoordinates, hit);
+            }
+
+            if (verticalInput < 0)
+            {
+                Vector2 destinationCellCoordinates = new Vector2(cellCenter.x, cellCenter.y - 1);
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, .5f, barrierLayer);
+                checkCollidersBeforeMoving(destinationCellCoordinates, hit);
+            }
+        }
+
+        void checkCollidersBeforeMoving(Vector2 destinationCellCoordinates, RaycastHit2D hit)
+        {
+            if (hit.collider == null)
+            {
+                moveTowardsNextCell(destinationCellCoordinates);
+            }
+        }
+
+        void moveTowardsNextCell(Vector2 adjacentCell)
+        {
+            Vector3 destinationCellCenter = BMTiles.GetCellCenter(adjacentCell, bg);
+            float distance = walkSpeed * Time.deltaTime;
+            entity.position = Vector3.MoveTowards(entity.position, destinationCellCenter, distance);
+        }
+    }
     
 }
